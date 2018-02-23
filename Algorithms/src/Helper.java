77521404c6de679cf37cf70d67171a1949696d63
Tolulope <to4g16@soton.ac.uk>
2018-02-23 10:44:14 +0000

    Insertion sort encore

---
+++ Algorithms/src/Helper.java
@@ -0,0 +1,13 @@
+
+public class Helper {
+	//Helper class with methods that are used often in several algorithms
+	//swap is an example
+
+	//array elements swapping method stolen from stackoverflow
+	public static void swap(int i, int j, int[] arr) {
+		   int t = arr[i];
+		   arr[i] = arr[j];
+		   arr[j] = t;
+		}
+	
+}
---
+++ Algorithms/src/InsertionSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+	
+	static int[] numbersToSort = {2,2,4,3,6,1};
+	String order = "ascending";
+	
+	public static void main(String[] args) {
+		insertionSort();
+		System.out.println(Arrays.toString(numbersToSort));
+
+	}
+	
+	public static int[] insertionSort() {
+		
+		for(int i=2; i < numbersToSort.length; i++) {
+			//For this for loop I think two in the psuedocode is 1 
+			//due to arrays starting with index 0
+			for(int j=i; j>=1; j--) {
+				if (numbersToSort[j] < numbersToSort[j-1]) {
+					Helper.swap(j, j-1, numbersToSort);
+				} else {
+					break;
+				}
+			}		
+			
+		}
+
+		return numbersToSort;
+	}
+
+}
